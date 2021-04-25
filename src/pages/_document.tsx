689e826006a6cacba307803b96296ce48304dc4b
João Vitor <joaovitormck@gmail.com>
2021-04-24 23:29:51 -0300

    stop zoom mobile

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
           <link rel="shortcut icon" href="/favicon.png" />
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
